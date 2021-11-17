abaf67ddd2d010fee2a95cdedac42ecea07a7c9c
Lorenzo <38138351+egoist000@users.noreply.github.com>
2021-11-18 00:16:42 +0100

    update animation timing

--- game.js
+++ game.js
@@ -111,7 +111,7 @@ const displayController = (() => {
             }
             setTimeout(() => {
                 cell.classList.remove("clean", "x", "o");
-            }, 300, cell);
+            }, 250, cell);
         }, 100, arrayLen);
     }
 
